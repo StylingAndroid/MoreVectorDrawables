79aa13dce47210e6d7a65c1fbe8978eaa84a8980
Mark Allison <mark@stylingandroid.com>
2012-04-14 12:06:02 +0100

    Part 2

--- src/com/stylingandroid/more/vector/MoreVectorDrawablesActivity.java
+++ src/com/stylingandroid/more/vector/MoreVectorDrawablesActivity.java
@@ -1,33 +1,15 @@
 package com.stylingandroid.more.vector;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
 public class MoreVectorDrawablesActivity extends Activity
 {
-	private BitmapDrawable background = null;
-
 	@Override
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.main );
-
-		ActionBar ab = getActionBar();
-		background = (BitmapDrawable) getResources().getDrawable(
-				R.drawable.backgroundpattern );
-		ab.setBackgroundDrawable( getResources().getDrawable(
-				R.drawable.backgroundpattern ) );
 	}
 
-	@Override
-	protected void onResume()
-	{
-		super.onResume();
-		final ActionBar actionBar = getActionBar();
-		background.setTileModeX( android.graphics.Shader.TileMode.REPEAT );
-		actionBar.setBackgroundDrawable( background );
-	}
 }
